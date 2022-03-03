e715e16a5c3df9c1485a30abca14f3175d68dd4b
Pectourus <69641403+Pectourus@users.noreply.github.com>
2022-03-02 19:32:59 -0500

    Add files via upload

---
+++ benrougelike/src/main/groovy/module-info.java
@@ -0,0 +1,12 @@
+module com.example.benrougelike {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.codehaus.groovy;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.benrougelike to javafx.fxml;
+    exports com.example.benrougelike;
+}
