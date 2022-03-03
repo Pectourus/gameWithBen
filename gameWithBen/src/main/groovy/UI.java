731f16567329053be2cdf07da03aeb62097c8a71
Pectourus <69641403+Pectourus@users.noreply.github.com>
2022-03-02 21:26:35 -0500

    Switched to maven

--- gameWithBen/src/main/groovy/UI.java
+++ gameWithBen/src/main/groovy/UI.java
@@ -1,14 +1,37 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class UI extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        try {
+            launch(args);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            System.exit(0);
+        }
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage mainStage) {
+        mainStage.setTitle("Test");
 
+        BorderPane root = new BorderPane();
+
+        Scene mainScene = new Scene(root);
+        mainStage.setScene(mainScene);
+
+        Canvas canvas = new Canvas();
+        GraphicsConfiguration context = canvas.getGraphicsConfiguration();
+
+        root.setCenter(canvas);
     }
 }
